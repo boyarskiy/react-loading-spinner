c4487625b52b8714affe6c803967da76231c863c
Eugeniy Boyarskiy <gekabrs@gmail.com>
2016-01-25 00:04:23 +0200

    update example

--- example/app/components/App.js
+++ example/app/components/App.js
@@ -2,39 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Content from './Content';
 import Spinner from './Spinner';
+
 import Loading from 'react-loading-spinner';
+import 'react-loading-spinner/src/css/index.css';
+
+import '../css/index.css';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      isLoading: false,
-      loadedContent: ''
+      customLoading: false,
+      customLoadingContent: '',
+      defaultLoading: false,
+      defaultLoadingContent: ''
     }
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleOnClick.bind(this)}>Start loading</button>
+        <div className='base'>
+          <button onClick={this.customLoadingHandler.bind(this)}
+                  className='btn'>Start custom loading</button>
 
-        <Loading isLoading={this.state.isLoading} loadingClassName={'loading'} spinner={Spinner}>
-          <Content loadedContent={this.state.loadedContent} />
-        </Loading>
+          <Loading isLoading={this.state.customLoading} loadingClassName='loading' spinner={Spinner}>
+            <Content loadedContent={this.state.customLoadingContent} />
+          </Loading>
+        </div>
 
+        <div className='base'>
+          <button onClick={this.defaultLoadingHandler.bind(this)}
+                  className='btn'>Start default loading</button>
+
+          <Loading isLoading={this.state.defaultLoading} loadingClassName='defloading'>
+            <Content loadedContent={this.state.defaultLoadingContent} />
+          </Loading>
+        </div>
       </div>
     );
   }
 
   // fetch imitation
-  handleOnClick() {
+  customLoadingHandler() {
     this.setState({
-      isLoading: true
+      customLoading: true
     });
     setTimeout(() => {
       this.setState({
-        isLoading: false,
-        loadedContent: 'Content loaded!'
+        customLoading: false,
+        customLoadingContent: 'Content loaded!'
+      })
+    }, 2000);
+  }
+
+  defaultLoadingHandler() {
+    this.setState({
+      defaultLoading: true
+    });
+    setTimeout(() => {
+      this.setState({
+        defaultLoading: false,
+        defaultLoadingContent: 'Content loaded!'
       })
     }, 2000);
   }
